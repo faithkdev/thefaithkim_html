2bda03ec45fa6918f1e6b21c856e27770c5918dc
faith <faithkdev@gmail.com>
2025-06-02 22:25:40 -0500

    dynamically show the read more button

--- js/projects/scripts.js
+++ js/projects/scripts.js
@@ -1,12 +1,41 @@
-const readMoreButtons = document.querySelectorAll('.read-more-btn');
-console.log({readMoreButtons});
-readMoreButtons.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        const content = btn.closest('.card');
-        console.log({content});
-        content.classList.toggle('expanded');
-        btn.textContent = content.classList.contains('expanded') ? 'Read less' : 'Read more';
+function checkOverflow(card) {
+    const contentArea = card.querySelector('.card-info');
+    const textArea = card.querySelector('.card-text');
+    const readMoreButton = card.querySelector('.read-more-btn');
+    const isOverflowing = textArea.clientHeight > contentArea.clientHeight;
+
+    console.log({isOverflowing, textArea: textArea.clientHeight, contentArea: contentArea.clientHeight})
+    if (isOverflowing) {
+        readMoreButton.style.display = 'inline-block';
+    } else {
+        readMoreButton.style.display = 'none';
+        card.classList.remove('expanded');
+        readMoreButton.textContent = 'Read more';
+    }
+}
+
+
+const cards = document.querySelectorAll('.card');
+
+
+function checkAllCards() {
+    cards.forEach((card) => {
+        const readMoreButton = card.querySelector('.read-more-btn');
+        readMoreButton.addEventListener('click', () => {
+            card.classList.toggle('expanded');
+            readMoreButton.textContent = card.classList.contains('expanded') ? 'Read less' : 'Read more';
+        });
+    
+        checkOverflow(card);
     });
+}
+
+let resizeTimeout;
+window.addEventListener('resize', () => {
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(() => {
+        cards.forEach(card => checkOverflow(card));
+    }, 200);
 });
 
-console.log({readMoreButtons});
+window.addEventListener('DOMContentLoaded', checkAllCards)
